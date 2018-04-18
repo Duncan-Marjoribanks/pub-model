40a7cb5d87e2cc1dbd2a71eb5a00bfda664a8473
Duncan <marjoribanks.d@gmail.com>
2018-04-18 14:52:26 +0100

    added age restriction to customer class

--- customer.rb
+++ customer.rb
@@ -17,12 +17,15 @@ class Customer
   end
 
   def buy_drink_from_pub(customer, pub, drink)
-
-    pub.remove_drink_from_stock(drink)
-    pub.add_to_till(drink.price)
-    customer.remove_money_from_wallet(drink.price)
+    if customer.age >= 18
+      pub.remove_drink_from_stock(drink)
+      pub.add_to_till(drink.price)
+      customer.remove_money_from_wallet(drink.price)
+    else
+      return nil
+    end
   end
-
+  
 
 
 
