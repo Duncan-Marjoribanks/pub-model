45dbfb07eb9f948cd2e4a3f4c8bc8702c0a7c894
Duncan <marjoribanks.d@gmail.com>
2018-04-18 13:25:02 +0100

    added remove a drink function to pub class

--- pub.rb
+++ pub.rb
@@ -7,7 +7,7 @@ attr_accessor(:till, :stock)
 def initialize(pub_name, till, stock)
   @pub_name = pub_name
   @till = till
-  @stock = stock 
+  @stock = stock
 end
 
 def add_drink_to_stock(drink)
@@ -15,10 +15,17 @@ def add_drink_to_stock(drink)
 end
 
 
+def remove_drink_from_stock(drink)
+  @stock.delete(drink)
+end
 
 
 
 
+
+# def check_pub_details(pub)
+#   p pub
+# end
 
 
 
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -8,7 +8,7 @@ class PubTest < MiniTest::Test
 def setup
   @drink_1 = Drink.new("Vodka", 3)
   @drink_2 = Drink.new("Rum", 4)
-  @pub_1 = Pub.new("The Sooky Dook", 100, [@drink_1])
+  @pub_1 = Pub.new("The Sooky Dook", 100, [@drink_1, @drink_2])
 end
 
 def test_has_name
@@ -22,16 +22,19 @@ def test_has_till
 end
 
 def test_can_add_drink
-  result = @pub_1.add_drink_to_stock(@drink_1)
-  assert_equal(2, result.count)
+  result = @pub_1.add_drink_to_stock(@drink_2)
+  assert_equal(3, result.count)
 end
 
+def test_remove_drink
+  result = @pub_1.remove_drink_from_stock(@drink_1)
+  assert_equal(1, @pub_1.stock.length)
+  assert_equal("Vodka", result.name)
+end
 
-# def test_has_drinks
-#   result = @pub_1.stock
-#   assert_equal()
-#
-#   end
+# def test_check_pub_details
+#   @pub_1.check_pub_details(@pub_1)
+# end
 
 
 
